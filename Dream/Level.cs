2e16047f4db86a9a415d4a7a83f456b763f95cfd
ilya.solomein <ilya.solomein@gmail.com>
2019-04-27 02:41:50 +0300

    Устал писать. Ужос.

---
+++ Dream/Level.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Dream
+{
+	public class Level : ILevel
+	{
+		public Point StartPlayerLocation { get; set; }
+		public List<Rectangle> Platforms { get; set; }
+		public Image LavelImage { get; set; }
+
+		public Level()
+		{
+			Platforms = new List<Rectangle>();
+			StartPlayerLocation = new Point(300, 300);
+		}
+
+		public void ExtractLavelFormFile(string path)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void DrawLavel(Graphics graphics)
+		{
+			graphics.DrawImage(LavelImage, new Point(0, 0));
+			throw new System.NotImplementedException();
+		}
+	}
+}
